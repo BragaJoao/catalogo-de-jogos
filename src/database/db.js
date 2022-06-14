180f1814a4e506fe780aab79f710d3c3ee036cb6
BragaJoao <joaogbraga@hotmail.com.br>
2022-06-13 22:01:10 -0300

    deploy

--- src/database/db.js
+++ src/database/db.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 
+/*
 const sequelize = new Sequelize(
   process.env.DB_BASE,
   process.env.DB_USER,
@@ -10,5 +11,18 @@ const sequelize = new Sequelize(
     dialect: "postgres",
   }
 );
+*/
+
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL,
+  {
+      dialect: "postgres",
+      dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+  },
+});
 
 module.exports = { sequelize };
